c9d3c7bb5497bf317f68024f6e5b9edb6650d16e
NitinBag0riya <nitin.naruto@gmail.com>
2019-01-27 14:57:43 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,12 +1,12 @@
 var Rebase = require('re-base');
 var firebase = require('firebase');
 var app = firebase.initializeApp({
-  apiKey: "AIzaSyCJzuNNOeeTEukHFbpjvMPrR4zI9KSTm-o",
-  authDomain: "gupshup-df8c5.firebaseapp.com",
-  databaseURL: "https://gupshup-df8c5.firebaseio.com",
-  projectId: "gupshup-df8c5",
-  storageBucket: "gupshup-df8c5.appspot.com",
-  messagingSenderId: "237645701062"
+  apiKey: "xxxxxx",
+  authDomain: "xxxxxx",
+  databaseURL: "xxxxxxx",
+  projectId: "xxxxxxx",
+  storageBucket: "xxxxxxxxx",
+  messagingSenderId: "xxxxxxxxx"
 });
 
 var base = Rebase.createClass(app.database());
